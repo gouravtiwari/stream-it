301c020906797b44f42bf606d84a485ff4e15f29
Gourav Tiwari <gouravtiwari21@gmail.com>
2016-08-17 11:09:45 -0700

    Add Rack::DeflaterWithExclusions to deflate html only for excluded URLs

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module StreamIt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.middleware.use 'Rack::DeflaterWithExclusions', :exclude => proc { |env|
+      env['PATH_INFO'].match(/\/posts\//) || env['PATH_INFO'].match(/\//(.)*/)
+    }
   end
 end
