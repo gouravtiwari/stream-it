ba5f4d7f94c66b3af04673edafd45c3d39f05d80
Gourav Tiwari <gouravtiwari21@gmail.com>
2016-08-18 15:53:30 -0700

    Exception handling in case of 500 errors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def display_posts?
+    # SomeComputation.show_posts?
+    true
+  rescue StandardError => e
+    Rails.logger.info e.message
+    Rails.logger.info e.backtrace.join("\n")
+    false
+  end
 end
